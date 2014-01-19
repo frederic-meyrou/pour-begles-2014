07ac42b4d87aee5c2c6a0a82327cc8f69060feea
Frederic Meyrou <frederic.meyrou@aboo.fr>
2014-01-19 01:33:58 +0100

    WP

--- wp-content/themes/ice-cap/page.php
+++ wp-content/themes/ice-cap/page.php
@@ -5,12 +5,6 @@
 
 
     <post><div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-      <?php if ( is_front_page() ) { ?>
-  		  <h4><?php the_title(); ?></h4>
-  	  <?php } else { ?>
-  		  <h4><?php the_title(); ?></h4>
-  	  <?php } ?>
-
       <p>
         <?php the_content(); ?>
         <?php edit_post_link( __( 'Edit', 'icecap' ), '<p class="edit-post">', '</p>' ); ?>
