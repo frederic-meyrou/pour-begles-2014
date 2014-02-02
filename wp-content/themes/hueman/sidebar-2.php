9e17c0b506ec246657ea22ed08a91610cbce88e8
Frederic Meyrou <frederic.meyrou@aboo.fr>
2014-02-02 16:02:38 +0100

    Mise en ligne du site

--- wp-content/themes/hueman/sidebar-2.php
+++ wp-content/themes/hueman/sidebar-2.php
@@ -8,7 +8,7 @@
 		
 		<div class="sidebar-top group">
 			<p><?php _e('RESEAUX','hueman'); ?></p>
-				<?php alx_social_links() ; ?>			
+				<?php alx_social_links() ; ?>	
 		</div>
 		
 		<?php if ( ot_get_option( 'post-nav' ) == 's2') { get_template_part('inc/post-nav'); } ?>
